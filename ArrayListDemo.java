9f268e871332fff6ce25378b092b03041fd21b59
Tanisha <tanisha.chaudhari2005@gmail.com>
2025-09-07 18:47:13 +0530

    Added ArrayList demo (collection framework start)

---
+++ ArrayListDemo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("core java");
+        list.add("Aptitude");
+        System.out.println(list);
+    }
+}
