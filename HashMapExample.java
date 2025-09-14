2a5db6690b810a5dce9f859efb9afec9aa0c1633
Tanisha <tanisha.chaudhari2005@gmail.com>
2025-09-14 12:58:57 +0530

    Hash Map Example added

---
+++ HashMapExample.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HashMapExample
+{
+    public static void main(String[] args) {
+        HashMap hm = new HashMap();
+        hm.put(null ,null);
+        hm.put(1,"Tanisha");
+        hm.put(2,"Pournima");
+        hm.put(3,"Monali");
+        hm.put(null ,null);
+        hm.put(4,"Revati");
+        System.out.println(hm);
+    }
+}
