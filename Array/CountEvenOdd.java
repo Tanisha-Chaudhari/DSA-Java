87ced0a1d491ec5873d7f760ab4cd8f67c081d90
Tanisha <tanisha.chaudhari2005@gmail.com>
2025-09-17 13:21:58 +0530

    Added count of even and odd element of array program

--- Array/CountEvenOdd.java
+++ Array/CountEvenOdd.java
@@ -1,2 +1,23 @@
-package Array;public class CountEvenOdd {
+package Array;
+
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        int[] arr = {12, 17, 18, 1,5,9,24,68};
+
+        int even =0;
+        int odd = 0;
+
+        for(int i=0; i< arr.length; i++){
+            if(arr[i] % 2 == 0) {
+                even++;
+            }
+            else{
+                odd++;
+
+            }
+        }
+        System.out.println("Count of even elements in array is: "+even);
+        System.out.println("Count of odd elements in array is: "+odd);
+
+    }
 }
